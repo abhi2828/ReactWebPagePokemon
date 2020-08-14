deedda8844fec0ae1fdec85ac0c99eb8e9aeb4b4
abhi2828 <abhi.mishra280896@gmail.com>
2020-08-14 20:57:54 +0530

    by ajay  create new branch  named dev

---
+++ build/precache-manifest.2bb622c0eee07f09df6149463559bc54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2fd80e5ee58831389e23feb565a74e",
+    "url": "/ReactWebPagePokemon/index.html"
+  },
+  {
+    "revision": "e849c43787b67103698d",
+    "url": "/ReactWebPagePokemon/static/css/main.1067bc3a.chunk.css"
+  },
+  {
+    "revision": "2bd31f9438cf3f74715a",
+    "url": "/ReactWebPagePokemon/static/js/2.c39ae040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWebPagePokemon/static/js/2.c39ae040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e849c43787b67103698d",
+    "url": "/ReactWebPagePokemon/static/js/main.4c70dc39.chunk.js"
+  },
+  {
+    "revision": "e75726b148e97bdbae9e",
+    "url": "/ReactWebPagePokemon/static/js/runtime-main.cad27c4f.js"
+  }
+]);
